a410da9656259ead9ce551f0ea34c563a6586c0a
MinnAK686 <anastasioak327@gmail.com>
2023-05-09 16:14:30 +0630

    Make registraion, logout and add middleware

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class AuthController extends Controller
 {
@@ -10,8 +12,35 @@ class AuthController extends Controller
     {
         return view("auth.register");
     }
+    public function store()
+    {
+        $formData = request()->validate([
+            "name" => [
+                "required", "min:3", "max:254"
+            ],
+            "username" => [
+                "required", "min:3", "max:254", Rule::unique("users", "username")
+            ],
+            "email" => [
+                "required", "email", "max:254", Rule::unique("users", "email")
+            ],
+            "password" => [
+                "required", "min:6", "max:254"
+            ]
+        ]);
+        $user = User::create($formData);
+        auth()->login($user);
+        return redirect("/")->with("success", "Welcome," . $user->name);
+    }
+
     public function login()
     {
         return view("auth.login");
+    }
+
+    public function logout()
+    {
+        auth()->logout();
+        return redirect("/")->with("success", "Bye, see you again");
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,14 @@
 <x-layout>
 	<div class="container" id="content">
+		@if (session('success'))
+			<div class="row">
+				<div class="col-10 col-sm-8 col-md-6 col-lg-4 mx-auto mt-4">
+					<div class="alert alert-success">
+						{{ session('success') }}
+					</div>
+				</div>
+			</div>
+		@endif
 		<h1 class="text-primary text-center mt-4 mb-2">Laravel Authentication System</h1>
 	</div>
 </x-layout>
